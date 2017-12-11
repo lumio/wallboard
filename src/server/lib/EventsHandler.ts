159b70e2f689f91dbd3e4925489957cc64f9bb26
Benjamin Falk <benjamin.falk@cyanjoe.com>
2017-12-11 11:33:04 +0100

    #8 Prepare events

--- src/server/lib/EventsHandler.ts
+++ src/server/lib/EventsHandler.ts
@@ -1,9 +1,49 @@
+function isUrl( s : string ) {
+  let check = true;
+  try {
+    new URL( s );
+  }
+  catch( e ) {
+    check = false;
+  }
+
+  return check;
+}
+
+function runCommandBlock( block : any, status : string ) {
+  if ( block.all ) {
+    console.log( 'all', block.all );
+  }
+
+  if ( block[ status ] ) {
+    console.log( status, block[ status ] );
+  }
+}
+
 function EventsHandler( config : any, jobName : string, building : boolean, status : string ) {
   const eventName = building ? 'build-start' : 'build-finish';
-  if ( !config.events || !config.events[ eventName ] || !config.events[ eventName ][ status ] ) {
+  const eventNameJobSpecific = `${ eventName }:${ jobName }`;
+  const oneEventExists = (
+    config.events
+    && (
+      ( config.events[ eventNameJobSpecific ] )
+      || ( config.events[ eventName ] )
+    )
+  );
+
+  if ( !oneEventExists ) {
     return;
   }
 
-  console.log( 'OIIIIDAAAA', config.events[ eventName ][ status ] );
+  const specificCommands = config.events[ eventNameJobSpecific ];
+  const genericCommands = config.events[ eventName ];
+
+  if ( specificCommands ) {
+    runCommandBlock( specificCommands, status );
+  }
+
+  if ( genericCommands ) {
+    runCommandBlock( genericCommands, status );
+  }
 }
 export default EventsHandler;
